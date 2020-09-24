a57ceb9c509277a10751ae4e29a047da5912fb0d
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-24 22:26:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,5 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  elseif int % 5 == 0
+    "Buzz"
+  elseif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  else
+    nil
   end
 end
